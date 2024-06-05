1c091183fe1fed70d206f7f03c9269ca17c7ec13
DanaSnellens <danasnellens@gmail.com>
2024-06-05 10:22:58 +0200

    Huiswerk week 12 af: Models:Television klasse af (attributen, constructors, getters & setters) + TelevisionRepository

---
+++ src/main/java/nl/novi/backend_hw11_techiteasy/repositories/TelevisionRepository.java
@@ -0,0 +1,8 @@
+package nl.novi.backend_hw11_techiteasy.repositories;
+import nl.novi.backend_hw11_techiteasy.models.Television;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+
+}
